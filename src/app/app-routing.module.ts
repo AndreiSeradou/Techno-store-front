d0654a1df84235129bf82b0b35269374392e9c1e
User <ziyodullayev2000@mail.ru>
2022-05-17 21:37:25 +0300

    add main window, services, catalog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
+import { MainWindowComponent } from './main-window/main-window.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"catalog",component:CatalogComponent},
+  {path:"main-window",component:MainWindowComponent},
+  {path:"services",component:ServicesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { MainWindowComponent } from './main-window/main-window.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogComponent,
+    MainWindowComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
